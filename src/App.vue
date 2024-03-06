ffb0fabea783addd8879094c793a86af01409264
zheer barzan <zheerbarzan85@outlook.com>
2024-03-06 14:15:30 +0300

    starting the design

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup>
+import { ref, } from 'vue';
+import { RouterLink, RouterView } from 'vue-router';
 
+import cheveronUP from 'vue-material-design-icons/ChevronUp.vue';
+import cheveronDown from 'vue-material-design-icons/ChevronDown.vue';
+import cheveronLeft from 'vue-material-design-icons/ChevronLeft.vue';
+import cheveronRight from 'vue-material-design-icons/ChevronRight.vue';
+
+let openMenu = ref(false);
 </script>
 
 <template>
