0256cd53d814ac759b6b566193b8a5f511736be4
zheer barzan <zheerbarzan85@outlook.com>
2024-03-06 14:05:34 +0300

    setting up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/app.vue"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
