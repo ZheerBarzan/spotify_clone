94eebc7fc85a42a3820014696ca0f8ba7b5669c6
zheer barzan <zheerbarzan85@outlook.com>
2024-03-09 22:06:22 +0300

    adding a picture

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,38 @@ let openMenu = ref(false);
 
 <template>
 
-  <div class="bg-red-400">cool</div>
+ <div>
+    <div class="w-[calc(100%-240px)]
+                h-[60px]
+                fixed
+                right-0
+                z-20
+                bg-[#101010]
+                bg-opacity-80
+                flex
+                items-center
+                justify-between">
+                
+                <div class="flex items-center ml-6">
+                    <button type="button" class="rounded-full bg-black p-[1px] cursor-pointer">
+
+                        <cheveronLeft :size="30" fillColor="white" />
+                    </button>
+                    <button type="button" class="rounded-full bg-black p-[1px] ml-4 cursor-pointer">
+
+                    <cheveronRight :size="30" fillColor="white" />
+                    </button>
+                </div>
+
+                <button class="bg-black hover:bg-[#282828] rounded-full cursor-pointer p-0.5 mr-8 mt-0.5">
+                  <div class="flex items-center">
+                    <img src="" alt="" class="rounded-full" width="27">
+                  </div>
+                </button>
+
+    </div>
+ </div>
+
   
 </template>
 
